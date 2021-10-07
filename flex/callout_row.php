22fb82b2865f1f6f2a7cf943406e65fffed7e71d
Dan Shields <dan@chips.nyc>
2021-10-07 15:36:43 -0400

    fix for callout row mismatch ID returned from CMS

--- flex/callout_row.php
+++ flex/callout_row.php
@@ -5,8 +5,10 @@ $scopedWrapInner = null;
 include(get_template_directory() . "/flex/_flex-header.php");
 
 foreach($cLoop['callouts'] as $co){
-	echo '
-	<a href="' . get_permalink($co) . '">' . get_the_title($co) . '</a>';
+	if(isset($co['target_page']) && !empty($co['target_page'])){
+		echo '
+	<a href="' . get_permalink($co['target_page']->ID) . '">' . get_the_title($co['target_page']->ID) . '</a>';
+	}
 }
 
 
